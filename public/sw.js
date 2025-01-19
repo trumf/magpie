6acbea1ce68bcfbd15c4abdfc52625133ab52e8e
Simon Rosenqvist <blundrs@gmail.com>
2025-01-19 16:10:50 +0700

    icons

--- public/sw.js
+++ public/sw.js
@@ -7,14 +7,9 @@ const STATIC_RESOURCES = [
   "/manifest.json",
   "/static/css/main.css",
   "/static/js/main.js",
-  "/icons/icon-72x72.png",
-  "/icons/icon-96x96.png",
-  "/icons/icon-128x128.png",
-  "/icons/icon-144x144.png",
-  "/icons/icon-152x152.png",
-  "/icons/icon-192x192.png",
-  "/icons/icon-384x384.png",
-  "/icons/icon-512x512.png",
+
+  "/icons/icon192.png",
+
   OFFLINE_URL,
 ];
 
